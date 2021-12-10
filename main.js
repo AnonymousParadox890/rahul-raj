4e3960cdd5a31d697b87a1eb862c5fb537745990
AnonymousParadox890 <abhyudaya890@gmail.com>
2021-12-10 18:12:20 +0530

    *

---
+++ main.js
@@ -0,0 +1,19 @@
+const showAlert = () => {
+  swal({
+    title: "@rio_rahul",
+    text: "Phele discord join kro\nVhir :P",
+    icon: "error",
+  });
+}
+
+const ChangeBackground = () => {
+var element = document.body;
+  element.classList.toggle("light-mode"); 
+}
+const showMsg = () => {
+  swal({
+    title:"@rio_rahul",
+    text:"Phele Join toh kro\nTab na link dunga :D", 
+    icon:"success"
+  })
+}
